c2543faf51753368a9da6e54ef2bf90e0fa98d18
fpena <facundopenia@gmail.com>
2023-12-03 12:46:40 -0300

    agregamos estilo al formulario y validacion mediante java script

---
+++ script.js
@@ -0,0 +1,24 @@
+function validacion(){
+var valor = document.getElementById("nombre").value;
+var valor2 = document.getElementById("correo").value;
+var valor3 = document.getElementById("asunto").value;
+var valor4 = document.getElementById("mensaje").value;
+if(valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+alert("El campo nombre está vacío");
+return false;
+}
+else if(valor2 == null || valor2.length == 0 || /^\s+$/.test(valor2)) {
+alert("El campo correo se encuentra vacío");
+return false;
+}
+
+else if(valor3 == null || valor3.length == 0 || /^\s+$/.test(valor3)) {
+alert("El campo asunto se encuentra vacío");
+return false;
+}
+else if(valor4 == null || valor4.length == 0 || /^\s+$/.test(valor4)) {
+alert("El campo mensaje se encuentra vacío");
+return false;
+}
+alert("Datos enviados");
+}
